e935a1ba4a8e4e150181c74e3f6cafa1105ac288
Timothy Tse <TimmyC@Timothys-MacBook-Air.local>
2016-09-12 17:39:12 -0400

    added download avatar file

---
+++ download_avatars.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var request = require('request');
+var data;
+
+
+//eltimsy:743c3c59703b9ba89297a3ea057da068ee5da01d
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request'
+    },
+    qs: {
+      'access_token': '743c3c59703b9ba89297a3ea057da068ee5da01d'
+    }
+  }
+
+   request(options, function(err, response, body) {
+     var avatar = [];
+     data = JSON.parse(body);
+     if (err) {
+       throw err;
+     } else {
+       for(var i = 0; i < data.length; i++) {
+         downloadImageByURL (data[i].avatar_url, 'avatars', data[i].login);
+         avatar.push(data[i].avatar_url);
+       }
+       cb(err, avatar);
+     }
+   })
+}
+
+function downloadImageByURL (url, filePath, i) {
+  request.get({url: url, encoding: 'binary'}, function (err, response, body) {
+    fs.writeFile(`avatars/${i}`, body, 'binary', function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log("The file was saved!");
+      };
+    });
+  });
+}
+//"danielsdeleo", "Decider"
+getRepoContributors(process.argv[2], process.argv[3], (err, result) => {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
