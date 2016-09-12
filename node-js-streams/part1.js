931e65590185e6ba3ac3d7394845cbad57d7c086
Timothy Tse <TimmyC@Timothys-MacBook-Air.local>
2016-09-12 12:05:55 -0400

    Added files

---
+++ node-js-streams/part1.js
@@ -0,0 +1,23 @@
+var http = require("http");
+
+var requestOptions = {
+  host: "google.com",
+  path: "/"
+};
+function printGoogleHTML(callback) {
+  http.get(requestOptions, (response) => {
+
+    response.setEncoding("utf8");
+
+    response.on("data", function(data) {
+      console.log(data);
+    });
+
+    response.on("end", function() {               
+      console.log("Response stream complete.");
+    });
+
+  });
+}
+
+printGoogleHTML();
